19c864c4817ff7cdacb354fe488629cc8f14958a
cilo <migayicecil@gmail.com>
2017-02-23 17:51:14 +0300

    changed the database name

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 
 <?php
-mysql_select_db('serena',mysql_connect('localhost','root',''))or die(mysql_error());
+mysql_select_db('benja',mysql_connect('localhost','root',''))or die(mysql_error());
 
 echo "connected";
 ?>
